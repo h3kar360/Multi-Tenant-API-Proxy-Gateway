f8e19c799a7b8300c07ec2ddd705d31cb330e497
h3kar360 <dericklievian@gmail.com>
2026-08-07 23:54:23 +0700

    made a more reasonable layout for obtaining proxy key in client

--- client/src/components/ProxyKey.tsx
+++ client/src/components/ProxyKey.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ProxyKeyTextBox from "./ProxyKeyTextBox";
 
 interface ProxyKeyValue {
-    proxyKey: string;
+  proxyKey: string;
 }
 
 const ProxyKey = ({ proxyKey }: ProxyKeyValue) => {
-    return (
-        <div className="flex flex-col justify-center items-center gap-10 mt-10">
-            <p className="text-6xl">Your API Key</p>
-            <ProxyKeyTextBox proxyKey={proxyKey} />
-        </div>
-    );
+  const [getProxyKey, setGetProxyKey] = useState(false);
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-10 mt-10">
+        <p className="text-6xl">Your API Key</p>
+        {getProxyKey ? (
+          <ProxyKeyTextBox proxyKey={proxyKey} />
+        ) : (
+          <button
+            onClick={() => setGetProxyKey(true)}
+            className="bg-gray-500 px-3 py-1 rounded hover:bg-gray-400"
+          >
+            Get Proxy Key
+          </button>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ProxyKey;
